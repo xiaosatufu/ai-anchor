5db0682563c0ca5169357011a912d3e20fbb7fb2
雍霜 <yonshuang.empty@dcits.com>
2025-03-18 14:04:39 +0800

    fix

--- src/pages/Server.vue
+++ src/pages/Server.vue
@@ -1,98 +1,100 @@
 <script setup lang="ts">
-
 import ServerStatus from "../components/Server/ServerStatus.vue";
-import {EnumServerType} from "../types/Server";
+import { EnumServerType } from "../types/Server";
 import ServerAddDialog from "../components/Server/ServerAddDialog.vue";
-import {onMounted, ref} from "vue";
-import {useServerStore} from "../store/modules/server";
-import {AppConfig} from "../config";
-import {t} from "../lang";
+import { onMounted, ref } from "vue";
+import { useServerStore } from "../store/modules/server";
+import { AppConfig } from "../config";
+import { t } from "../lang";
 import ServerActionDelete from "../components/Server/ServerActionDelete.vue";
 import ServerActionStartStop from "../components/Server/ServerActionStartStop.vue";
 import ServerActionLog from "../components/Server/ServerActionLog.vue";
 import ServerStartTime from "../components/Server/ServerStartTime.vue";
 import ServerActionInfo from "../components/Server/ServerActionInfo.vue";
 import ServerActionSetting from "../components/Server/ServerActionSetting.vue";
-import {functionToLabels} from "../lib/aigcpanel";
+import { functionToLabels } from "../lib/aigcpanel";
 
-const addDialog = ref<InstanceType<typeof ServerAddDialog> | null>(null)
-const serverStore = useServerStore()
-const helpShow = ref(false)
+const addDialog = ref<InstanceType<typeof ServerAddDialog> | null>(null);
+const serverStore = useServerStore();
+const helpShow = ref(false);
 
 const doHelp = () => {
-    window.$mapi.app.openExternalWeb(AppConfig.helpUrl)
-}
+  window.$mapi.app.openExternalWeb(AppConfig.helpUrl);
+};
 
 const doRefresh = async () => {
-    await serverStore.refresh()
-}
+  await serverStore.refresh();
+};
 
 const typeName = (type: string) => {
-    if (EnumServerType.LOCAL === type) {
-        return t('本地模型')
-    } else if (EnumServerType.LOCAL_DIR === type) {
-        return t('本地模型目录')
-    } else if (EnumServerType.CLOUD === type) {
-        return t('云端模型')
-    }
-}
-
+  if (EnumServerType.LOCAL === type) {
+    return t("本地模型");
+  } else if (EnumServerType.LOCAL_DIR === type) {
+    return t("本地模型目录");
+  } else if (EnumServerType.CLOUD === type) {
+    return t("云端模型");
+  }
+};
 </script>
 
 <template>
-    <div class="pb-device-container p-8 min-h-full relative select-none">
-        <div class="mb-4 flex items-center">
-            <div class="text-3xl font-bold flex-grow">
-                {{ $t('模型') }}
-            </div>
-            <div class="flex items-center">
-                <a-button v-if="serverStore.records.length>0"
-                          class="ml-1"
-                          @click="addDialog?.show()">
-                    <template #icon>
-                        <icon-plus/>
-                    </template>
-                    {{ $t('添加模型服务') }}
-                </a-button>
-                <a-button v-if="0" class="ml-1">
+  <div class="relative p-8 min-h-full select-none pb-device-container">
+    <div class="flex items-center mb-4">
+      <div class="flex-grow text-3xl font-bold">
+        {{ $t("模型") }}
+      </div>
+      <div class="flex items-center">
+        <a-button
+          v-if="serverStore.records.length > 0"
+          class="ml-1"
+          @click="addDialog?.show()"
+        >
+          <template #icon>
+            <icon-plus />
+          </template>
+          {{ $t("添加模型服务") }}
+        </a-button>
+        <!-- <a-button v-if="0" class="ml-1">
                     <template #icon>
                         <icon-apps/>
                     </template>
                     {{ $t('添加云端模型') }}
-                </a-button>
-            </div>
+                </a-button> -->
+      </div>
+    </div>
+    <div class="-mx-2">
+      <div v-if="!serverStore.records.length" class="py-20">
+        <div class="text-center">
+          <img
+            class="m-auto h-32 opacity-50"
+            src="./../assets/image/server-empty.svg"
+          />
         </div>
-        <div class="-mx-2">
-            <div v-if="!serverStore.records.length" class="py-20">
-                <div class="text-center">
-                    <img class="h-32 m-auto opacity-50" src="./../assets/image/server-empty.svg"/>
-                </div>
-                <div class="mt-5 text-center text-gray-400">
-                    <div>{{ $t('暂时还没有模型，请添加模型~') }}</div>
-                </div>
-                <div class="mt-5 text-center">
-                    <a-button class="ml-1"
-                              @click="addDialog?.show()">
-                        <template #icon>
-                            <icon-plus/>
-                        </template>
-                        {{ $t('添加模型服务') }}
-                    </a-button>
-                    <a-button v-if="0" class="ml-1">
-                        <template #icon>
-                            <icon-apps/>
-                        </template>
-                        {{ $t('添加云端模型') }}
-                    </a-button>
-                    <a-button class="mx-1" @click="helpShow=true">
-                        <template #icon>
-                            <icon-book class="mr-1"/>
-                        </template>
-                        {{ $t('如何添加模型？') }}
-                    </a-button>
-                </div>
-                <div v-if="helpShow" class="pt-5 text-center">
-                    <div class="inline-block bg-gray-100 text-left rounded-lg p-6 leading-8">
+        <div class="mt-5 text-center text-gray-400">
+          <div>{{ $t("暂时还没有模型，请添加模型~") }}</div>
+        </div>
+        <div class="mt-5 text-center">
+          <a-button class="ml-1" @click="addDialog?.show()">
+            <template #icon>
+              <icon-plus />
+            </template>
+            {{ $t("添加模型服务") }}
+          </a-button>
+          <a-button v-if="0" class="ml-1">
+            <template #icon>
+              <icon-apps />
+            </template>
+            {{ $t("添加云端模型") }}
+          </a-button>
+          <!-- <a-button class="mx-1" @click="helpShow = true">
+            <template #icon>
+              <icon-book class="mr-1" />
+            </template>
+            {{ $t("如何添加模型？") }}
+          </a-button> -->
+        </div>
+        <!-- <div v-if="helpShow" class="pt-5 text-center">
+                    <div class="inline-block p-6 leading-8 text-left bg-gray-100 rounded-lg">
                         <div>① {{ $t('访问官方模型页面，下载模型到本地') }}</div>
                         <div>② {{ $t('解压模型压缩包，选择目录中的config.json文件') }}</div>
                         <div class="pt-3">
@@ -103,66 +105,81 @@ const typeName = (type: string) => {
                             </a>
                         </div>
                     </div>
-                </div>
-            </div>
-            <div v-else class="flex flex-wrap">
-                <div v-for="record in serverStore.records"
-                     class="w-full lg:w-1/2 2xl:w-1/3 p-3">
-                    <div class="rounded-xl shadow border p-4">
-                        <div class="flex items-center">
-                            <div class="flex-grow">
-                                <div class="inline-block mr-4">
-                                    <a-tooltip :content="typeName(record.type as string)">
-                                        <div class="inline-block">
-                                            <i v-if="record.type===EnumServerType.CLOUD"
-                                               class="iconfont icon-network"></i>
-                                            <i v-else-if="record.type===EnumServerType.LOCAL"
-                                               class="iconfont icon-desktop"></i>
-                                            <i v-else-if="record.type===EnumServerType.LOCAL_DIR"
-                                               class="iconfont icon-folder"></i>
-                                        </div>
-                                    </a-tooltip>
-                                </div>
-                                <div class="inline-block mr-4">
-                                    {{ record.title }}
-                                    <div class="inline-block rounded-3xl bg-gray-100 px-3">
-                                        v{{ record.version }}
-                                    </div>
-                                </div>
-                            </div>
-                            <div>
-                                <ServerStatus :status="record.status"/>
-                            </div>
-                        </div>
-                        <div class="h-12 pt-4">
-                            <div class="text-gray-400 text-sm">
-                                <a-tag v-for="label in functionToLabels(record.functions)" class="mr-1">
-                                    {{ label }}
-                                </a-tag>
-                            </div>
-                        </div>
-                        <div class="pt-4 flex items-center">
-                            <div class="flex-grow">
-                                <ServerActionStartStop v-if="record.type===EnumServerType.LOCAL_DIR"
-                                                       :record="record"/>
-                                <ServerActionLog :record="record"/>
-                                <ServerActionDelete :record="record" @update="doRefresh"/>
-                                <ServerActionInfo :record="record"/>
-                                <ServerActionSetting v-if="record.settings && Object.keys(record.settings).length>0"
-                                                     :record="record"/>
-                            </div>
-                            <div>
-                                <ServerStartTime :record="record"/>
-                            </div>
-                        </div>
+                </div> -->
+      </div>
+      <div v-else class="flex flex-wrap">
+        <div
+          v-for="record in serverStore.records"
+          class="p-3 w-full lg:w-1/2 2xl:w-1/3"
+        >
+          <div class="p-4 rounded-xl border shadow">
+            <div class="flex items-center">
+              <div class="flex-grow">
+                <div class="inline-block mr-4">
+                  <a-tooltip :content="typeName(record.type as string)">
+                    <div class="inline-block">
+                      <i
+                        v-if="record.type === EnumServerType.CLOUD"
+                        class="iconfont icon-network"
+                      ></i>
+                      <i
+                        v-else-if="record.type === EnumServerType.LOCAL"
+                        class="iconfont icon-desktop"
+                      ></i>
+                      <i
+                        v-else-if="record.type === EnumServerType.LOCAL_DIR"
+                        class="iconfont icon-folder"
+                      ></i>
                     </div>
+                  </a-tooltip>
                 </div>
+                <div class="inline-block mr-4">
+                  {{ record.title }}
+                  <div class="inline-block px-3 bg-gray-100 rounded-3xl">
+                    v{{ record.version }}
+                  </div>
+                </div>
+              </div>
+              <div>
+                <ServerStatus :status="record.status" />
+              </div>
             </div>
+            <div class="pt-4 h-12">
+              <div class="text-sm text-gray-400">
+                <a-tag
+                  v-for="label in functionToLabels(record.functions)"
+                  class="mr-1"
+                >
+                  {{ label }}
+                </a-tag>
+              </div>
+            </div>
+            <div class="flex items-center pt-4">
+              <div class="flex-grow">
+                <ServerActionStartStop
+                  v-if="record.type === EnumServerType.LOCAL_DIR"
+                  :record="record"
+                />
+                <ServerActionLog :record="record" />
+                <ServerActionDelete :record="record" @update="doRefresh" />
+                <ServerActionInfo :record="record" />
+                <ServerActionSetting
+                  v-if="
+                    record.settings && Object.keys(record.settings).length > 0
+                  "
+                  :record="record"
+                />
+              </div>
+              <div>
+                <ServerStartTime :record="record" />
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
-    <ServerAddDialog ref="addDialog" @update="doRefresh"/>
+  </div>
+  <ServerAddDialog ref="addDialog" @update="doRefresh" />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/pages/Setting.vue
+++ src/pages/Setting.vue
@@ -1,92 +1,104 @@
 <script setup lang="ts">
-import {onBeforeUnmount, onMounted, ref} from 'vue'
-import {t} from '../lang'
-import {TabContentScroller} from "../lib/ui";
+import { onBeforeUnmount, onMounted, ref } from "vue";
+import { t } from "../lang";
+import { TabContentScroller } from "../lib/ui";
 import SettingBasic from "../components/Setting/SettingBasic.vue";
 import SettingEnv from "../components/Setting/SettingEnv.vue";
 import SettingAbout from "../components/Setting/SettingAbout.vue";
 
-let tabContentScroller: TabContentScroller | null = null
-const contentContainer = ref<HTMLElement | null>(null)
-const tabContainer = ref<HTMLElement | null>(null)
+let tabContentScroller: TabContentScroller | null = null;
+const contentContainer = ref<HTMLElement | null>(null);
+const tabContainer = ref<HTMLElement | null>(null);
 onMounted(() => {
-    tabContentScroller = new TabContentScroller(
-        tabContainer.value as HTMLElement,
-        contentContainer.value as HTMLElement,
-        {
-            activeClass: 'menu-active'
-        }
-    )
-})
+  tabContentScroller = new TabContentScroller(
+    tabContainer.value as HTMLElement,
+    contentContainer.value as HTMLElement,
+    {
+      activeClass: "menu-active",
+    },
+  );
+});
 onBeforeUnmount(() => {
-    tabContentScroller?.destroy()
-})
-
+  tabContentScroller?.destroy();
+});
 </script>
 
 <style lang="less" scoped>
 .menu-active {
-    --tw-bg-opacity: 1;
-    background-color: rgb(243 244 246 / var(--tw-bg-opacity));
+  --tw-bg-opacity: 1;
+  background-color: rgb(243 244 246 / var(--tw-bg-opacity));
 }
 
 [data-theme="dark"] {
-    .menu-active {
-        background-color: var(--color-bg-page-nav-active);;
-    }
+  .menu-active {
+    background-color: var(--color-bg-page-nav-active);
+  }
 }
 </style>
 
 <template>
-    <div class="flex select-none">
-        <div ref="tabContainer"
-             class="p-8 w-56 flex-shrink-0 border-r border-solid border-gray-100 dark:border-gray-600">
-            <div data-section="basic" class="p-2 rounded-lg mr-2 mb-4 cursor-pointer menu-active">
-                <div class="text-base">
-                    <icon-settings/>
-                    {{ t('基础设置') }}
-                </div>
-            </div>
-            <div v-if="0" data-section="env" class="p-2 rounded-lg mr-2 mb-4 cursor-pointer">
-                <div class="text-base">
-                    <icon-code/>
-                    {{ t('环境设置') }}
-                </div>
-            </div>
-            <div data-section="about" class="p-2 rounded-lg mr-2 mb-4 cursor-pointer">
-                <div class="text-base">
-                    <icon-user/>
-                    {{ t('关于软件') }}
-                </div>
-            </div>
+  <div class="flex select-none">
+    <div
+      ref="tabContainer"
+      class="flex-shrink-0 p-8 w-56 border-r border-gray-100 border-solid dark:border-gray-600"
+    >
+      <div
+        data-section="basic"
+        class="p-2 mr-2 mb-4 rounded-lg cursor-pointer menu-active"
+      >
+        <div class="text-base">
+          <icon-settings />
+          {{ t("基础设置") }}
         </div>
-        <div class="flex-grow">
-            <div ref="contentContainer"
-                 class="overflow-y-auto p-8 leading-8"
-                 style="height:calc(100vh - var(--window-header-height));">
-                <div data-section="basic" class="scroll-mt-4">
-                    <div class="text-base font-bold mb-4">{{ t('基础设置') }}</div>
-                    <div>
-                        <SettingBasic/>
-                    </div>
-                </div>
-                <div v-if="0" class="border-b border-solid border-gray-200 my-6"></div>
-                <div v-if="0" data-section="env" class="scroll-mt-4">
-                    <div class="text-base font-bold mb-4">{{ t('环境设置') }}</div>
-                    <div>
-                        <SettingEnv/>
-                    </div>
-                </div>
-                <div class="border-b border-solid border-gray-200 my-6 dark:border-gray-700"></div>
-                <div data-section="about" class="scroll-mt-4">
-                    <div class="text-base font-bold mb-4">
+      </div>
+      <div
+        v-if="0"
+        data-section="env"
+        class="p-2 mr-2 mb-4 rounded-lg cursor-pointer"
+      >
+        <div class="text-base">
+          <icon-code />
+          {{ t("环境设置") }}
+        </div>
+      </div>
+      <div data-section="about" class="p-2 mr-2 mb-4 rounded-lg cursor-pointer">
+        <div class="text-base">
+          <icon-user />
+          {{ t("关于软件") }}
+        </div>
+      </div>
+    </div>
+    <div class="flex-grow">
+      <div
+        ref="contentContainer"
+        class="overflow-y-auto p-8 leading-8"
+        style="height: calc(100vh - var(--window-header-height))"
+      >
+        <div data-section="basic" class="scroll-mt-4">
+          <div class="mb-4 text-base font-bold">{{ t("基础设置") }}</div>
+          <div>
+            <SettingBasic />
+          </div>
+        </div>
+        <div v-if="0" class="my-6 border-b border-gray-200 border-solid"></div>
+        <div v-if="0" data-section="env" class="scroll-mt-4">
+          <div class="mb-4 text-base font-bold">{{ t("环境设置") }}</div>
+          <div>
+            <SettingEnv />
+          </div>
+        </div>
+        <div
+          class="my-6 border-b border-gray-200 border-solid dark:border-gray-700"
+        ></div>
+        <!-- <div data-section="about" class="scroll-mt-4">
+                    <div class="mb-4 text-base font-bold">
                         {{ t('关于软件') }}
                     </div>
                     <div class="">
                         <SettingAbout/>
                     </div>
-                </div>
-            </div>
-        </div>
+                </div> -->
+      </div>
     </div>
+  </div>
 </template>
